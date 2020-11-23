2f5ead51f673e7643f4c93a30b06185bc2d54848
Max le prof <masamune.code@gmail.com>
2020-11-23 16:37:21 +0100

    🍱ADD: Carre > bouton carre visible

--- src/components/Carre.js
+++ src/components/Carre.js
@@ -6,6 +6,31 @@ class Carre extends React.Component {
 
     // On définit un état afin de savoir si le carré est visible
     this.state = { isCarreVisible: true };
+
+    // On relie this pour éviter qu'il soit 'undefined' dans la fonction 'handleCarreVisibleClick'
+    this.handleCarreVisibleClick = this.handleCarreVisibleClick.bind(this);
+  }
+
+  handleCarreVisibleClick() {
+    console.log('handleCarreVisibleClick()');
+    
+    // Si c'était vrai, ca devient faux
+    // Si c'était faux, ca devient vrai
+    /*
+    const ancienIsCarreVisible = this.state.isCarreVisible;
+    let nouveauIsCarreVisible;
+    if(ancienIsCarreVisible === true) {
+      nouveauIsCarreVisible = false;
+    }
+    else {
+      nouveauIsCarreVisible = true;
+    }
+    this.setState({ isCarreVisible: nouveauIsCarreVisible });
+    */
+
+    // Même chose
+    // La nouvelle valeur de l'état, c'est l'inverse/l'opposé de l'ancienne
+    this.setState({ isCarreVisible: !this.state.isCarreVisible });
   }
 
   // Dans le rendu
@@ -25,6 +50,7 @@ class Carre extends React.Component {
     return (
           <div>
             {elementCarre}
+            <button onClick={this.handleCarreVisibleClick}>{ isCarreVisible ? 'On me voit' : 'On me voit pas'}</button>
             <button>rouge</button>
             <button>bleu</button>
           </div>
