a8e78ebdcdd9367d4a29d40ceffc4c3ffc322a1c
Max le prof <masamune.code@gmail.com>
2020-11-23 16:21:59 +0100

    ✨MOD: Carre > ajout du state

--- src/components/Carre.js
+++ src/components/Carre.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 class Carre extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // On définit un état afin de savoir si le carré est visible
+    this.state = { isCarreVisible: true };
+  }
+
+  // Dans le rendu
   render() {
+    // Avant l'affichage, on gère ...
+    const isCarreVisible = this.state.isCarreVisible;
+    let elementCarre;
+
+    // ... L'affichage (ou non) du carré
+    //    https://fr.reactjs.org/docs/conditional-rendering.html#element-variables
+    if (isCarreVisible) {
+      elementCarre = <div className="carre"></div>;
+    } else {
+      elementCarre = null;
+    }
+
     return (
           <div>
-            <div className="carre"></div>
-            <button>On me voit</button>
+            {elementCarre}
             <button>rouge</button>
             <button>bleu</button>
           </div>
