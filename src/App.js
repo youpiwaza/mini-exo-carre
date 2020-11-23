273a41eefc33d0a016c506dd78e768f092daa9da
Max le prof <masamune.code@gmail.com>
2020-11-23 15:59:21 +0100

    🎉ADD: Mini exo > carre

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Carre from './components/Carre';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mon super projet de carré de ouf</h1>
+
+        <Carre></Carre>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Carre.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Carre extends React.Component {
+  render() {
+    return (
+          <div>
+            <div className="carre"></div>
+            <button>On me voit</button>
+            <button>rouge</button>
+            <button>bleu</button>
+          </div>
+    );
+  }
+}
+
+export default Carre;
